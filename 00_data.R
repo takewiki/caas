14888994a9255bf9c52384b17ea3b41a1f02dc49
takewiki <hulilei@takewiki.com.cn>
2020-04-27 18:29:11 +0800

    add the baseline

--- 00_data.R
+++ 00_data.R
@@ -1,9 +1,9 @@
 # 设置app标题-----
 
-app_title <-'App Template';
+app_title <-'AI汽车导购综合平台';
 
 # store data into rdbe in the rds database
-app_id <- 'appTpl'
+app_id <- 'caas'
 
 conn <- conn_rds('rdbe')
 
